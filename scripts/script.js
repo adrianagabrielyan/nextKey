cff682050d6d3c6915772851826c50d6c1031059
Adriana Gabrielyan <adrin.gabrielyan97@mail.ru>
2018-05-19 13:13:35 +0400

    aaa
    aaa

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,49 @@
 var button = document.getElementById('nextKey');
 var rooms = document.getElementsByClassName('room');
 
-function emptyRoom() {
-     if (i % 2 == 0) {
-    for (i = 0; i <= 9; i++) {
+function findIndex() {
+    var i = 0;
+    for (i = 0; i <= 9; i += 2) {
+
         if (rooms[i].className.indexOf('busy') === -1) {
             return i;
         }
 
         if (i + 2 < rooms.length && rooms[i + 2].className.indexOf('busy') === -1) {
             return i + 2;
-        } 
+        }
+
     }
+    if (i == 10 ) {
+        for (i = 1; i <= 9; i += 2) {
 
+            if (rooms[i].className.indexOf('busy') === -1) {
+                return i;
+            }
 
-          //  if (i + 1 < rooms.length && rooms[i + 1].className.indexOf('busy') === -1) {
-              //  return i + 1;
-          //  }
-
+            if (i + 2 < rooms.length && rooms[i + 2].className.indexOf('busy') === -1) {
+                return i + 2;
+            }
+        }
     }
 }
-
 function coloringRoom() {
-    var nextIndex = emptyRoom();
+    var nextIndex = findIndex();
     var i = 0;
     while (i <= 9) {
         document.getElementById('room' + nextIndex).classList.add('busy');
         i++;
         break;
     }
-}
-var isButtonDisabled = true;
-for (i = 0; i <= 9; i++) {
-    if (document.getElementById('room' + i).className.indexOf('busy') === -1) {
-        isButtonDisabled = false;
-        break;
+    var isButtonDisabled = true;
+    for (i = 0; i <= 9; i++) {
+        if (document.getElementById('room' + i).className.indexOf('busy') === -1) {
+            isButtonDisabled = false;
+            break;
+        }
     }
+    button.disabled = isButtonDisabled;
 }
-button.disabled = isButtonDisabled;
 
 function freeRoom() {
 
@@ -50,7 +56,8 @@ function freeRoom() {
         isButtonAbled = false;
 
     }
-    button.disabled = isButtonAbled;;
+    button.disabled = isButtonAbled;
+
 }
 
 button.onclick = coloringRoom;
